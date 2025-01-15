27b171707e840cb6c2de20fd1bf6ffaaa579878c
Venusstars1 <nkohavi2028@kehillahstudent.org>
2025-01-15 12:00:39 -0800

    Finished Decrypting

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import string
 
 alphabet = string.ascii_lowercase
-print(alphabet)
+#print(alphabet)
 
-password = "hi potato zzz"
+secret_message = "y qc q iushuj cuiiqwu oek mybb duluh wkuii"
 # eve wants to steal my password
 
 # make my password secret
-key = 4
 
-def encrypt(plaintext):
-    ciphertext = ""
-    for letter in plaintext:
+#print(encrypt(password))
+def decrypt(key):
+    decrypted_message = ""
+    for letter in secret_message:
         old_position = alphabet.find(letter)
         if old_position == -1:
-            ciphertext += " "
+            decrypted_message += " "
         else:
-            new_position = old_position + key
+            new_position = old_position - key
             new_position = new_position % len(alphabet)
             new_letter = alphabet[new_position]
-            ciphertext += new_letter
-    return ciphertext
+            decrypted_message += new_letter
+    return decrypted_message
 
-print(encrypt(password))
+
+key = 0
+for i in range(26):
+    print(decrypt(key))
+    key += 1
+#Key is 16
+
 
 # Your task:
 # figure out what key I used to encrypt this message:
-secret_message = "y qc q iushuj cuiiqwu oek mybb duluh wkuii"
+#secret_message = "y qc q iushuj cuiiqwu oek mybb duluh wkuii"
